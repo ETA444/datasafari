6166df6cb2a0c77df656be6d3bb7923756b496cb
George <georgedreemer@proton.me>
2024-02-18 18:48:43 +0100

    Create explorer subpackage __init__.py

---
+++ datasafari/explorer/__init__.py
@@ -0,0 +1,7 @@
+"""Subpackage for exploration functionalities within Data Safari."""
+
+# import all explorers
+from .explore_df import explore_df
+from .explore_cat import explore_cat
+# from .explore_num import explore_num - TODO: create explore_num()
+
