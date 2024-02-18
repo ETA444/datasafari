513ceac56f4447d12a4aec0bb1dd54d1cf9e71c9
George <georgedreemer@proton.me>
2024-02-18 18:48:15 +0100

    Create top-level __init__.py

--- datasafari/__init__.py
+++ datasafari/__init__.py
@@ -3,3 +3,12 @@
 __author__ = """George Dreemer"""
 __email__ = 'georgedreemer@proton.me'
 __version__ = '0.1.0'
+
+# import core functionality
+
+# explorers
+from .explorer import (
+    explore_df, explore_cat, explore_num
+)
+
+# TODO: add more later
