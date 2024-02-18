3620e2b5a8170538b946eaaebbeb3311707b2f2a
George <georgedreemer@proton.me>
2024-02-18 12:00:42 +0100

    Create explore_cat: prelim structure & functionality

---
+++ src/explorer/explore_cat.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+
+# main function: explore_cat
+def explore_cat(
+        df: pd.DataFrame,
+        categorical_variables: list,
+        method: str = 'all',
+        output: str = 'print'):
+    # docstring here
+    result = []
+
+    if method.lower() in ['unique', 'all']:
+        # initial append for title of method section
+        result.append(f"<<______UNIQUE VALUES PER VARIABLE______>>\n")
+
+        # get the unique values per variable in categorical_variables list
+        for variable_name in categorical_variables:
+            unique_values = df[variable_name].unique()
+            result.append(f"['{variable_name}'] Unique values:\n{unique_values}")
+
+    # Combine all results
+    combined_result = "\n".join(result)
+
+    if output.lower() == 'print':
+        print(combined_result)
+    elif output.lower() == 'return':
+        return combined_result
+    else:
+        raise ValueError("Invalid output method. Choose 'print' or 'return'.")
+
+
+# smoke test
+test_df = pd.read_csv('./data/soil_measures.csv')
+variables_of_interest = ['crop']
+unique = explore_cat(
+    df=test_df,
+    categorical_variables=variables_of_interest,
+    method='unique',
+    output='return'
+)
