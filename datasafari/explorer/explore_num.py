dbf6a6bc776b2bcbc4a9d60788cc44fabba38df7
George <georgedreemer@proton.me>
2024-02-24 18:24:13 +0100

    Function variables and output structure

--- datasafari/explorer/explore_num.py
+++ datasafari/explorer/explore_num.py
@@ -4,7 +4,16 @@ import pandas as pd
 # main function: explore_num
 def explore_num(df: pd.DataFrame, numerical_variables: list, method: str = 'all', output: str = 'print'):
     """..."""
+
+    # initialize variables #
+    # (1) each method appends to: result
     result = []
+    # (2) method 'outliers_zscore' only, returns these to the user
+    outliers_z_dict = {}
+    outliers_z_df = pd.DataFrame()
+    # (3) method 'outliers_iqr' only, returns these to the user
+    outliers_iqr_dict = {}
+    outliers_iqr_df = pd.DataFrame()
 
     if method.lower() in ['outlier_z', 'all']:
         # initial append for title of method section
@@ -36,8 +45,12 @@ def explore_num(df: pd.DataFrame, numerical_variables: list, method: str = 'all'
                 ][variable_name].tolist()
             )
 
-            # save results to oid dictionary
-            outliers_z[variable_name] = oid_values
+            # save results to outliers_z_dict
+            outliers_z_dict[variable_name] = outlier_z_values
+
+            # save resulting rows to outliers_z_df
+            outlier_rows = df[df[variable_name].isin(outliers_z_dict[variable_name])]
+            outliers_z_df = pd.concat([outliers_z_df, outlier_rows], ignore_index=False)
 
             # Drop z-score column
             # penguins_clean = df_z.drop(columns=[z_col])
@@ -54,8 +67,15 @@ def explore_num(df: pd.DataFrame, numerical_variables: list, method: str = 'all'
 
     if output.lower() == 'print':
         print(combined_result)
+
+        if method.lower() == 'outliers_zscore':
+            return outliers_z_dict, outliers_z_df
+
     elif output.lower() == 'return':
-        return combined_result
+
+        if method.lower() == 'outliers_zscore':
+            return outliers_z_dict, outliers_z_df
+
     else:
         raise ValueError("Invalid output method. Choose 'print' or 'return'.")
 
