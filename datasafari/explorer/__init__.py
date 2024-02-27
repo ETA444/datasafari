bea4f2066a53ff2f9dc57d7c455f6c35a91beb80
George <georgedreemer@proton.me>
2024-02-27 12:21:43 +0100

    Update init files

--- datasafari/explorer/__init__.py
+++ datasafari/explorer/__init__.py
@@ -1,6 +1,5 @@
 """Subpackage for exploration functionalities within Data Safari."""
 
-# import all explorers
 from .explore_df import explore_df
 from .explore_cat import explore_cat
 from .explore_num import explore_num
