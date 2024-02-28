f9e782b4a72b04049d6f01aa384c99175b73465f
George <georgedreemer@proton.me>
2024-02-28 15:51:47 +0100

    Construct console output of new method: clean_uniform

--- datasafari/transformer/transform_cat.py
+++ datasafari/transformer/transform_cat.py
@@ -1,23 +1,48 @@
 import pandas as pd
 from sklearn.preprocessing import (
-    StandardScaler, OneHotEncoder
+    OneHotEncoder,
+    StandardScaler
 )
 from datasafari import explore_cat
 
 
 # main function: transform_cat
-def transform_cat(df: pd.DataFrame, categorical_variables: list, method: str = 'all', output: str = 'print'):
+def transform_cat(df: pd.DataFrame, categorical_variables: list, method: str):
     # TODO: preprocess categorical data to make: uniform (e.g. Education column has University and university, etc.)
     # TODO: easily create ordinal categorical variables
     # TODO: easily create nominal categorical variables
     # TODO: ordinal encoding with overwrite or no overwrite
     # TODO: one hot encoding
 
+    if method.lower() == 'clean_uniform':
+        print(f"< MAKING CATEGORIES UNIFORM: {categorical_variables} >")
+        print(f"  ✔ Fix capitalization: e.g. ['Student', 'STUDENT', 'stUdent'] => ['student']")
+        print(f"  ✔ Fix whitespace: e.g. ['high   school', 'high school  ', '  high school'] => ['high school']")
+
+        # initialize dfs to work with
+        transformed_df = df.copy()
+        uniform_columns = pd.DataFrame()
+
+        # main functionality
+        for variable in categorical_variables:
+
+            print(f"\n['{variable}']\n")
+            print(f"Categories BEFORE: {df[variable].unique()}\n")
+            print(f"Categories AFTER: {transformed_df[variable].unique()}\n")
+
+        # inform user
+        print(f"✔ Created a transformed dataframe:\n{transformed_df.head()}\n\n✔ Created a dataframe with only the uniform columns:\n{uniform_columns.head()}\n\n☻ HOW TO: transformed_df, uniform_columns = transform_cat(yourdf, yourcolumns, method='clean_uniform'\n")
+
+        # sanity check
+        print(f"< SANITY CHECK >\n  ➡ Shape of original df: {df.shape}\n  ➡ Shape of transformed df: {transformed_df.shape}\n")
+
+        return transformed_df, uniform_columns
+
     if method.lower() == 'encode_onehot':
         print(f"< ONE-HOT ENCODING: {categorical_variables} >\n✎ Note: Please make sure your data is cleaned.\n☻ Tip: You can use explore_cat() to check your data!\n")
 
         # initialize df to work with
-        transformed_df = df
+        transformed_df = df.copy()
 
         # create an instance of OneHotEncoder
         onehot_encoder = OneHotEncoder(sparse=False)
