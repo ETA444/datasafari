20827263b10ccfe64ea71821ca212a62a578ed74
George <georgedreemer@proton.me>
2024-03-04 16:57:14 +0100

    Add console output for method: 'standardize'

--- datasafari/transformer/transform_num.py
+++ datasafari/transformer/transform_num.py
@@ -37,6 +37,11 @@ def transform_num(df, numerical_variables, method, **kwargs):
     # TODO: Add console output for standardize
 
     if method == 'standardize':
+        print(f"< STANDARDIZING DATA >")
+        print(f" This method centers the data around mean 0 with a standard deviation of 1, enhancing model performance and stability.")
+        print(f"  ✔ Standardizes each numerical variable to have mean=0 and variance=1.")
+        print(f"  ✔ Essential preprocessing step for many machine learning algorithms.\n")
+        print(f"✎ Note: Standardization is applied only to the specified numerical variables.\n")
 
         # initialize essential objects
         transformed_df = df.copy()
@@ -47,6 +52,15 @@ def transform_num(df, numerical_variables, method, **kwargs):
 
         # isolate transformed columns to give as part of output
         standardized_columns = transformed_df[numerical_variables]
+
+        print(f"✔ New transformed dataframe:\n{transformed_df.head()}\n")
+        print(f"✔ Dataframe with only the scaled columns:\n{standardized_columns.head()}\n")
+        print("☻ HOW TO - to catch the dfs: `transformed_df, standardized_columns = transform_num(your_df, your_numerical_variables, method='standardize')`.\n")
+
+        # sanity check
+        print("< SANITY CHECK >")
+        print(f"  ➡ Shape of original dataframe: {df.shape}")
+        print(f"  ➡ Shape of transformed dataframe: {transformed_df.shape}\n")
 
         return transformed_df, standardized_columns
 
