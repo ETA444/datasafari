1d4adb523529a22d65d14654fbe7568986b39fdb
George <georgedreemer@proton.me>
2024-03-13 18:43:02 +0100

    Update explore_df() docstring: examples and raises
    - Write better example section which is reproducable without need for the user to have data ready
    - Add raises section which details all errors the function raises

--- datasafari/explorer/explore_df.py
+++ datasafari/explorer/explore_df.py
@@ -48,30 +48,56 @@ def explore_df(df: pd.DataFrame, method: str = 'all', output: str = 'print', **k
     str or None
         String if output='return'. Otherwise, prints to console and returns None.
 
+    Raises
+    ------
+    TypeError
+        If the `df` parameter is not a pandas DataFrame. This ensures that the function is applied to the correct data type.
+
+    ValueError
+        - If an invalid 'method' option is provided. This error informs users about the acceptable values for the 'method' parameter.
+        - If 'buf' parameter is used in the 'info' method. This error is specific to the limitation that 'buf' parameter cannot be used within the 'info' method in `explore_df`.
+        - If an invalid 'output' option is provided. This error guides users to choose only from the supported options for 'output'.
+
     Examples
     --------
-    # Summary statistics with custom percentiles
-    >>> explore_df(df, 'desc', percentiles=[0.05, 0.95])
+    # Create a sample DataFrame to use in the examples:
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> data = {
+    ...     'A': np.random.randn(100),
+    ...     'B': np.random.rand(100) * 100,
+    ...     'C': np.random.randint(1, 100, size=100),
+    ...     'D': np.random.choice(['X', 'Y', 'Z'], size=100)
+    ... }
+    >>> df = pd.DataFrame(data)
+
+    # The power of this function lies in 'all' method, simply:
+    >>> explore_df(df, 'all')
+
+    # Alternatively you can save the output to text and even to a file
+    >>> summary = explore_df(df, 'all', output='return')
+
+    # Display summary statistics with custom percentiles (thanks to **kwargs filtering!)
+    >>> explore_df(df, 'desc', percentiles=[0.05, 0.95], output='print')
 
     # Display the first 3 rows
-    >>> explore_df(df, 'head', n=3)
+    >>> explore_df(df, 'head', n=3, output='print')
 
     # Detailed DataFrame information
-    >>> explore_df(df, 'info', verbose=True)
+    >>> explore_df(df, 'info', verbose=True, output='print')
 
     # Count and percentage of missing values
-    >>> explore_df(df, 'na')
+    >>> explore_df(df, 'na', output='print')
 
-    # Comprehensive exploration with custom settings
+    # Comprehensive exploration with custom settings, displaying to console
     >>> explore_df(df, 'all', n=3, percentiles=[0.25, 0.75], output='print')
 
     # Returning comprehensive exploration results as a string
     >>> result_str = explore_df(df, 'all', n=5, output='return')
     >>> print(result_str)
 
-    # Using 'all' with kwargs that apply to specific methods
-    # Note: kwargs that don't apply to a specific method are ignored by that method
-    >>> explore_df(df, 'all', n=5, percentiles=[0.1, 0.9], verbose=False)
+    # Using 'all' with kwargs that apply to specific methods, printing the results:
+    >>> explore_df(df, 'all', n=5, percentiles=[0.1, 0.9], verbose=False, output='print')
     """
 
     # Error Handling #
@@ -85,7 +111,6 @@ def explore_df(df: pd.DataFrame, method: str = 'all', output: str = 'print', **k
     if 'buf' in kwargs and method.lower() == 'info':
         raise ValueError("'buf' parameter is not supported in the 'info' method within explore_df.")
 
-    # Additional error checking for output
     if output.lower() not in ['print', 'return']:
         raise ValueError("Invalid output method. Choose 'print' or 'return'.")
 
