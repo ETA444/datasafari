8912d22a7e3079dfe22d5af6fa16d0a9e0414131
George <georgedreemer@proton.me>
2024-03-14 15:28:57 +0100

    Define function basic schema;

--- datasafari/predictor/predict_hypothesis.py
+++ datasafari/predictor/predict_hypothesis.py
@@ -9,4 +9,19 @@ def predict_hypothesis():
     and so on
     (4) It handles the key datatypes categorical and numeric automatically
 
+    Schema of the idea:
+    > In > user data (df and ??)
+    | 1 Inside: infer numeric or categorical
+    \\
+     \\
+        | 2A Inside: if numeric test assumptions to choose if parametric or non-parametric
+        | 3A Inside: perform either set of tests and get results
+        | 4A Inside: use results to build output and make a ready conclusion for the user
+        < Out < results + conclusion
+    \\
+     \\
+        | 2B Inside: if categorical  ...
+        | 3B Inside: ...
+        | 4B Inside: ...
+        < Out < results + conclusion
     """
