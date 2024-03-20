281d07f88a666c0001b417f989abc3e19bc63be4
George <georgedreemer@proton.me>
2024-03-20 14:11:28 +0100

    Implement categorical hypothesis testing
    - first test frequencies in the contingency table [testing_frquencies()]
    - then test shape of the contingency table [testing_shape()]
    - then perform robust checks and determine the most appropriate set of tests for the data
    - write comprehensive console output with tips, justifications and more
    - reusable and easy to understand output format
    - smoke tested: success

--- datasafari/predictor/predict_hypothesis.py
+++ datasafari/predictor/predict_hypothesis.py
@@ -165,3 +165,132 @@ def perform_hypothesis_testing_numeric(df, target_variable, grouping_variable, n
 
 perform_hypothesis_testing_numeric(test_df, variable, grouping, normality_info, equal_variances_info)
 
+
+def testing_frequencies(df, categorical_variable1, categorical_variable2):
+    # initialize contingency table
+    contingency_table = pd.crosstab(df[categorical_variable1], df[categorical_variable2])
+    # compute minimum expected and observed frequencies
+    min_expected_frequency = expected_freq(contingency_table).min()
+    min_observed_frequency = contingency_table.min().min()
+    # test assumption of chi2_contingency
+    chi2_bool = False
+    if min_expected_frequency >= 5 and min_observed_frequency >= 5:
+        chi2_bool = True
+    return chi2_bool, contingency_table
+
+
+def testing_shape(contingency_table):
+    barnard_bool = False
+    boschloo_bool = False
+    fisher_bool = False
+    yates_correction_shape_bool = False  # note: related to chi2_contingency parameter 'correction'
+    if contingency_table.shape == (2,2):  # if table is 2x2 various tests become viable
+        barnard_bool = True
+        boschloo_bool = True
+        fisher_bool = True
+        yates_correction_shape_bool = True
+    return barnard_bool, boschloo_bool, fisher_bool, yates_correction_shape_bool
+
+
+def perform_hypothesis_testing_categorical(contingency_table, chi2_bool, barnard_bool, boschloo_bool, fisher_bool, yates_correction_shape_bool, alternative: str = 'two-sided', yates_min_sample_size: int = 40):
+    # to avoid unnecessary parameter inputs get var names from contingency_table object
+    categorical_variable1 = contingency_table.index.name
+    categorical_variable2 = contingency_table.columns.name
+    min_expected_frequency = expected_freq(contingency_table).min()
+    min_observed_frequency = contingency_table.min().min()
+    contingency_table_shape = contingency_table.shape
+    sample_size = np.sum(contingency_table.values)
+
+    # define output
+    output_info = []
+
+    if chi2_bool:
+        if yates_correction_shape_bool and sample_size <= yates_min_sample_size:
+            stat, p_val, dof, expected_frequencies = chi2_contingency(contingency_table, correction=True)
+            test_name = f"Chi-square test of independence of variables (with Yate's Correction)"
+            chi2_tip = f"\n☻ Tip: The Chi-square test of independence with Yates' Correction is used for 2x2 contingency tables with small sample sizes. Yates' Correction makes the test more conservative, reducing the Type I error rate by adjusting for the continuity of the chi-squared distribution. This correction is typically applied when sample sizes are small (often suggested for total sample sizes less than about 40), aiming to avoid overestimation of statistical significance."
+            conclusion = f"There is no statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {p_val:.3f}, with Yate's Correction)." if p_val > 0.05 else f"There is a statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {p_val:.3f}, with Yate's Correction)."
+            chi2_output_info = {'stat': stat, 'p_val': p_val, 'test_name': test_name, 'tip': chi2_tip, 'conclusion': conclusion}
+            output_info.append(chi2_output_info)
+        else:
+            stat, p_val, dof, expected_frequencies = chi2_contingency(contingency_table, correction=False)
+            test_name = f"Chi-square test of independence of variables (without Yate's Correction)"
+            chi2_tip = f"\n☻ Tip: The Chi-square test of independence without Yates' Correction is preferred when analyzing larger contingency tables or when sample sizes are sufficiently large, even for 2x2 tables (often suggested for total sample sizes greater than 40). Removing Yates' Correction can increase the test's power by not artificially adjusting for continuity, making it more sensitive to detect genuine associations between variables in settings where the assumptions of the chi-squared test are met."
+            conclusion = f"There is no statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {p_val:.3f}, without Yate's Correction)." if p_val > 0.05 else f"There is a statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {p_val:.3f}, without Yate's Correction)."
+            chi2_output_info = {'stat': stat, 'p_val': p_val, 'test_name': test_name, 'tip': chi2_tip, 'conclusion': conclusion}
+            output_info.append(chi2_output_info)
+    else:
+        if barnard_bool:
+            barnard_test = barnard_exact(contingency_table, alternative=alternative.lower())
+            barnard_stat = barnard_test.statistic
+            barnard_p_val = barnard_test.pvalue
+            bernard_test_name = f"Barnard's exact test ({alternative.lower()})"
+            bernard_tip = f"\n☻ Tip: Barnard's exact test is often preferred for its power, especially in unbalanced designs or with small sample sizes, without the need for the continuity correction that Fisher's test applies."
+            if alternative.lower() == 'two-sided':
+                bernard_conclusion = f"There is no statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {barnard_p_val:.3f})." if barnard_p_val > 0.05 else f"There is a statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {barnard_p_val:.3f})."
+            elif alternative.lower() == 'less':
+                bernard_conclusion = f"The data do not support a statistically significant decrease in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {barnard_p_val:.3f})." if barnard_p_val > 0.05 else f"The data support a statistically significant decrease in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {barnard_p_val:.3f})."
+            elif alternative.lower() == 'greater':
+                bernard_conclusion = f"The data do not support a statistically significant increase in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {barnard_p_val:.3f})." if barnard_p_val > 0.05 else f"The data support a statistically significant increase in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {barnard_p_val:.3f})."
+
+            # consolidate info for output
+            bernard_output_info = {'stat': barnard_stat, 'p_val': barnard_p_val, 'test_name': bernard_test_name, 'tip': bernard_tip, 'conclusion': bernard_conclusion}
+            output_info.append(bernard_output_info)
+
+        if boschloo_bool:
+            boschloo_test = boschloo_exact(contingency_table, alternative=alternative.lower())
+            boschloo_stat = boschloo_test.statistic
+            boschloo_p_val = boschloo_test.pvalue
+            boschloo_test_name = f"Boschloo's exact test ({alternative.lower()})"
+            boschloo_tip = f"\n☻ Tip: Boschloo's exact test is an extension that increases power by combining the strengths of Fisher's and Barnard's tests, focusing on the most extreme probabilities."
+            if alternative.lower() == 'two-sided':
+                boschloo_conclusion = f"There is no statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {boschloo_p_val:.3f})." if boschloo_p_val > 0.05 else f"There is a statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {boschloo_p_val:.3f})."
+            elif alternative.lower() == 'less':
+                boschloo_conclusion = f"The data do not support a statistically significant decrease in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {boschloo_p_val:.3f})." if boschloo_p_val > 0.05 else f"The data support a statistically significant decrease in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {boschloo_p_val:.3f})."
+            elif alternative.lower() == 'greater':
+                boschloo_conclusion = f"The data do not support a statistically significant increase in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {boschloo_p_val:.3f})." if boschloo_p_val > 0.05 else f"The data support a statistically significant increase in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {boschloo_p_val:.3f})."
+
+            # consolidate info for ouput
+            boschloo_output_info = {'stat': boschloo_stat, 'p_val': boschloo_p_val, 'test_name': boschloo_test_name, 'tip': boschloo_tip, 'conclusion': boschloo_conclusion}
+            output_info.append(boschloo_output_info)
+
+        if fisher_bool:
+            fisher_test = fisher_exact(contingency_table, alternative=alternative.lower())
+            fisher_stat = fisher_test[0]
+            fisher_p_val = fisher_test[1]
+            fisher_test_name = f"Fisher's exact test ({alternative.lower()})"
+            fisher_tip = f"\n☻ Tip: Fisher's exact test is traditionally used for small sample sizes, providing exact p-values under the null hypothesis of independence."
+            if alternative.lower() == 'two-sided':
+                fisher_conclusion = f"There is no statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {fisher_p_val:.3f})." if fisher_p_val > 0.05 else f"There is a statistically significant association between {categorical_variable1} and {categorical_variable2} (p = {fisher_p_val:.3f})."
+            elif alternative.lower() == 'less':
+                fisher_conclusion = f"The data do not support a statistically significant decrease in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {fisher_p_val:.3f})." if fisher_p_val > 0.05 else f"The data support a statistically significant decrease in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {fisher_p_val:.3f})."
+            elif alternative.lower() == 'greater':
+                fisher_conclusion = f"The data do not support a statistically significant increase in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {fisher_p_val:.3f})." if fisher_p_val > 0.05 else f"The data support a statistically significant increase in the frequency of {categorical_variable1} compared to {categorical_variable2} (p = {fisher_p_val:.3f})."
+
+            # consolidate info for output
+            fisher_output_info = {'stat': fisher_stat, 'p_val': fisher_p_val, 'test_name': fisher_test_name, 'tip': fisher_tip, 'conclusion': fisher_conclusion}
+            output_info.append(fisher_output_info)
+
+    # console output & return
+    generate_test_names = (output_info[0]['test_name'] if len(output_info) == 1 else [info['test_name'] for info in output_info])
+    print(f"< HYPOTHESIS TESTING: {generate_test_names}>\nBased on:\n  ➡ Sample size: {sample_size}\n  ➡ Minimum Observed Frequency: {min_observed_frequency}\n  ➡ Minimum Expected Frequency: {min_expected_frequency}\n  ➡ Contingency table shape: {contingency_table_shape[0]}x{contingency_table_shape[1]}\n  ∴ Performing {generate_test_names}:\n")
+    [print(f"Results of {info['test_name']}:\n  ➡ statistic: {info['stat']}\n  ➡ p-value: {info['p_val']}\n  ∴ Conclusion: {info['conclusion']}{info['tip']}\n") for info in output_info]
+    return output_info
+
+
+# testing exact tests
+perform_hypothesis_testing_categorical(contingency_table_test2x2, False, True, True, True, True)
+
+# testing chi2 with yates
+perform_hypothesis_testing_categorical(contingency_table_test2x2sample39, True, False, False, False, True)
+
+# testing chi2 without yates
+perform_hypothesis_testing_categorical(contingency_table_test3x3, True, False, False, False, False)
+
+
+    # DONE! TODO: Handle returns of numerical
+    # TODO: Error handling for all
+    # TOOD: Enrich numerical testing
+    # TODO: Enrich parameters of internal functions
+    # TODO: Construct main function
+    # TODO: Write Numpy Docstring for main function
