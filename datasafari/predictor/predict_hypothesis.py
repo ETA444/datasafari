616951a4792dda3061dc017397cc842ab1e1f5e6
George <georgedreemer@proton.me>
2024-03-26 18:22:51 +0100

    Update evaluate_shape() utility function

--- datasafari/predictor/predict_hypothesis.py
+++ datasafari/predictor/predict_hypothesis.py
@@ -21,242 +21,39 @@ def evaluate_data_types(df, cols):
     return data_type_dictionary
 
 
-dt_dict = assign_data_types(test_df, test_df.columns)
+def evaluate_frequencies(df: pd.DataFrame, categorical_variable1: str, categorical_variable2: str):
+    """Evaluates whether expected and observed frequencies are adequate for chi2_contingency()."""
+
+    # initialize contingency table
+    contingency_table = pd.crosstab(df[categorical_variable1], df[categorical_variable2])
+    # compute minimum expected and observed frequencies
+    min_expected_frequency = expected_freq(contingency_table).min()
+    min_observed_frequency = contingency_table.min().min()
+    # test assumption of chi2_contingency
+    chi2_bool = False
+    if min_expected_frequency >= 5 and min_observed_frequency >= 5:
+        chi2_bool = True
+    return chi2_bool, contingency_table
 
 
-def evaluate_normality(df, target_variable, grouping_variable, method: str = 'consensus', pipeline: bool = False):
-    groups = df[grouping_variable].unique().tolist()
+def evaluate_shape(contingency_table):
+    """Evaluates whether the shape of the contingency table is suitable and for what tests."""
 
-    # define output objects
-    output_info = {}  # non-pipeline
-    normality_info = {}  # pipeline single-method
-    output_consensus = None  # pipeline consensus method
-
-    if method in ['shapiro', 'consensus']:
-        # calculating statistic and p-values to define normality
-        shapiro_stats = [shapiro(df[df[grouping_variable] == group][target_variable]).statistic for group in groups]
-        shapiro_pvals = [shapiro(df[df[grouping_variable] == group][target_variable]).pvalue for group in groups]
-        shapiro_normality = [True if p > 0.05 else False for p in shapiro_pvals]
-
-        # save the info for return and text for output
-        shapiro_info = {group: {'stat': shapiro_stats[n], 'p': shapiro_pvals[n], 'normality': shapiro_normality[n]} for n, group in enumerate(groups)}
-        shapiro_text = [f"Results for '{key}' group in variable ['{target_variable}']:\n  ➡ statistic: {value['stat']}\n  ➡ p-value: {value['p']}\n{(f'  ∴ Normality: Yes (H0 cannot be rejected)' if value['normality'] else f'  ∴ Normality: No (H0 rejected)')}\n\n" for key, value in shapiro_info.items()]
-        shapiro_title = f"< NORMALITY TESTING: SHAPIRO-WILK >\n\n"
-        shapiro_tip = "☻ Tip: The Shapiro-Wilk test is particularly well-suited for small sample sizes (n < 50) and is considered one of the most powerful tests for assessing normality. It is sensitive to departures from normality, making it a preferred choice for rigorous normality assessments in small datasets.\n"
-
-        # save info in return object and conditionally print to console
-        output_info['shapiro'] = shapiro_info
-        normality_info['shapiro_group_consensus'] = all(shapiro_normality)
-
-        # end it here if non-consensus method
-        if method == 'shapiro':
-            print(shapiro_title, *shapiro_text, shapiro_tip)
-            return output_info if not pipeline else normality_info['shapiro_group_consensus']
-
-    if method in ['anderson', 'consensus']:
-        # calculating statistic to define normality
-        anderson_stats = [anderson(df[df[grouping_variable] == group][target_variable]).statistic for group in groups]
-        anderson_critical_values = [anderson(df[df[grouping_variable] == group][target_variable]).critical_values[2] for group in groups]
-        anderson_normality = [True if c_val > anderson_stats[n] else False for n, c_val in enumerate(anderson_critical_values)]
-
-        # save the info for return and text for output
-        anderson_info = {group: {'stat': anderson_stats[n], 'p': anderson_critical_values[n], 'normality': anderson_normality[n]} for n, group in enumerate(groups)}
-        anderson_text = [f"Results for '{key}' group in variable ['{target_variable}']:\n  ➡ statistic: {value['stat']}\n  ➡ p-value: {value['p']}\n{(f'  ∴ Normality: Yes (H0 cannot be rejected)' if value['normality'] else f'  ∴ Normality: No (H0 rejected)')}\n\n" for key, value in anderson_info.items()]
-        anderson_title = f"< NORMALITY TESTING: ANDERSON-DARLING >\n\n"
-        anderson_tip = "☻ Tip: The Anderson-Darling test is a versatile test that can be applied to any sample size and is especially useful for comparing against multiple distribution types, not just the normal. It places more emphasis on the tails of the distribution than the Shapiro-Wilk test, making it useful for detecting outliers or heavy-tailed distributions.\n"
-
-        # saving info
-        output_info['anderson'] = anderson_info
-        normality_info['anderson_group_consensus'] = all(anderson_normality)
-
-        # end it here if non-consensus method
-        if method == 'anderson':
-            print(anderson_title, *anderson_text, anderson_tip)
-            return output_info if not pipeline else normality_info['anderson_group_consensus']
-
-    if method in ['normaltest', 'consensus']:
-        # calculating statistic and p-values to define normality
-        normaltest_stats = [normaltest(df[df[grouping_variable] == group][target_variable]).statistic for group in groups]
-        normaltest_pvals = [normaltest(df[df[grouping_variable] == group][target_variable]).pvalue for group in groups]
-        normaltest_normality = [True if p > 0.05 else False for p in normaltest_pvals]
-
-        # save the info for return and text for output
-        normaltest_info = {group: {'stat': normaltest_stats[n], 'p': normaltest_pvals[n], 'normality': normaltest_normality[n]} for n, group in enumerate(groups)}
-        normaltest_text = [f"Results for '{key}' group in variable ['{target_variable}']:\n  ➡ statistic: {value['stat']}\n  ➡ p-value: {value['p']}\n{(f'  ∴ Normality: Yes (H0 cannot be rejected)' if value['normality'] else f'  ∴ Normality: No (H0 rejected)')}\n\n" for key, value in normaltest_info.items()]
-        normaltest_title = f"< NORMALITY TESTING: D'AGOSTINO-PEARSON NORMALTEST >\n\n"
-        normaltest_tip = "☻ Tip: The D'Agostino-Pearson normality test, or simply 'normaltest', is best applied when the sample size is larger, as it combines skewness and kurtosis to form a test statistic. This test is useful for detecting departures from normality that involve asymmetry and tail thickness, offering a good balance between sensitivity and specificity in medium to large sample sizes.\n"
-
-        # saving info
-        output_info['normaltest'] = normaltest_info
-        normality_info['normaltest_group_consensus'] = all(normaltest_normality)
-
-        # end it here if non-consensus method
-        if method == 'normaltest':
-            print(normaltest_title, *normaltest_text, normaltest_tip)
-            return output_info if not pipeline else normality_info['normaltest_group_consensus']
-
-    if method in ['lilliefors', 'consensus']:
-        # calculating statistic and p-values to define normality
-        lilliefors_stats = [lilliefors(df[df[grouping_variable] == group][target_variable])[0] for group in groups]
-        lilliefors_pvals = [lilliefors(df[df[grouping_variable] == group][target_variable])[1] for group in groups]
-        lilliefors_normality = [True if p > 0.05 else False for p in lilliefors_pvals]
-
-        # save the info for return and text for output
-        lilliefors_info = {group: {'stat': lilliefors_stats[n], 'p': lilliefors_pvals[n], 'normality': lilliefors_normality[n]} for n, group in enumerate(groups)}
-        lilliefors_text = [f"Results for '{key}' group in variable ['{target_variable}']:\n  ➡ statistic: {value['stat']}\n  ➡ p-value: {value['p']}\n{(f'  ∴ Normality: Yes (H0 cannot be rejected)' if value['normality'] else f'  ∴ Normality: No (H0 rejected)')}\n\n" for key, value in lilliefors_info.items()]
-        lilliefors_title = f"< NORMALITY TESTING: LILLIEFORS' TEST >\n\n"
-        lilliefors_tip = "☻ Tip: The Lilliefors test is an adaptation of the Kolmogorov-Smirnov test for normality with the benefit of not requiring the mean and variance to be known parameters. It's particularly useful for small to moderately sized samples and is sensitive to deviations from normality in the center of the distribution rather than the tails. This makes it complementary to tests like the Anderson-Darling when a comprehensive assessment of normality is needed.\n"
-
-        # saving info
-        output_info['lilliefors'] = lilliefors_info
-        normality_info['lilliefors_group_consensus'] = all(lilliefors_normality)
-
-        # end here if non-consensus method
-        if method == 'lilliefors':
-            print(lilliefors_title, *lilliefors_text, lilliefors_tip)
-            return output_info if not pipeline else normality_info['lilliefors_group_consensus']
-
-    if method == 'consensus':
-        # the logic is that more than half of the tests need to give True for the consensus to be True
-        true_count = [group_normality_consensus for test_name, group_normality_consensus in normality_info.items()].count(True)
-        false_count = [group_normality_consensus for test_name, group_normality_consensus in normality_info.items()].count(False)
-        half_point = len(normality_info)/2
-        if true_count > half_point:
-            consensus_percent = (true_count / len(normality_info)) * 100
-            normality_consensus_result = f"  ➡ Result: Consensus is reached.\n  ➡ {consensus_percent}% of tests suggest Normality. *\n\n* Detailed results of each test are provided below.\n"
-
-            # used only within predict_hypothesis() pipeline
-            output_consensus = True
-
-        elif true_count < half_point:
-            consensus_percent = (false_count / len(normality_info)) * 100
-            normality_consensus_result = f"  ➡ Result: Consensus is reached.\n  ➡ {consensus_percent}% of tests suggest Non-Normality. *\n\n* Detailed results of each test are provided below:\n"
-
-            # used only within predict_hypothesis() pipeline
-            output_consensus = False
-
-        elif true_count == half_point:
-            # the logic is that we default to the decisions of shapiro-wilk and anderson-darling (if both true then true) - internal use only
-            normality_consensus_result = f"  ➡ Result: Consensus is not reached. (50% Normality / 50% Non-normality)\n\n∴ Please refer to the results of each test below:\n"
-
-            # used only within predict_hypothesis() pipeline
-            output_consensus = all([normality_info['shapiro_group_consensus'], normality_info['anderson_group_consensus']])
-
-        # construct output and return
-        print(f"< NORMALITY TESTING: CONSENSUS >\nThe consensus method bases its conclusion on 4 tests: Shapiro-Wilk test, Anderson-Darling test, D'Agostino-Pearson test, Lilliefors test. (Note: More than 50% must have the same outcome to reach consensus. In predict_hypothesis(), if consensus is not reached normality is settled based on Shapiro-Wilk and Anderson-Darling outcomes)\n\n{normality_consensus_result}")
-        print(shapiro_title, *shapiro_text, shapiro_tip)
-        print(anderson_title, *anderson_text, anderson_tip)
-        print(normaltest_title, *normaltest_text, normaltest_tip)
-        print(lilliefors_title, *lilliefors_text, lilliefors_tip)
-        return output_info if not pipeline else output_consensus
+    barnard_bool = False
+    boschloo_bool = False
+    fisher_bool = False
+    yates_correction_shape_bool = False  # note: related to chi2_contingency parameter 'correction'
+    if contingency_table.shape == (2,2):  # if table is 2x2 various tests become viable
+        barnard_bool = True
+        boschloo_bool = True
+        fisher_bool = True
+        yates_correction_shape_bool = True
+    return barnard_bool, boschloo_bool, fisher_bool, yates_correction_shape_bool
 
 
-evaluate_normality(test_df, variable, grouping, method='lilliefors', pipeline=True)
+def predict_hypo_num(df, target_variable, grouping_variable, normality_bool, equal_variances_bool):
+    """Runs tests for a hypothesis for numerical data."""
 
-
-def evaluate_variance(df, target_variable, grouping_variable, normality_info: bool = None, method: str = 'consensus', pipeline: bool = False):
-    groups = df[grouping_variable].unique().tolist()
-    samples = [df[df[grouping_variable] == group][target_variable] for group in groups]
-
-    # define output objects
-    output_info = {}  # non-pipeline
-    variance_info = {}  # pipeline single-method
-    output_consensus = None  # pipeline consensus method
-
-    if method in ['levene', 'consensus']:
-        levene_stat, levene_pval = levene(*samples).statistic, levene(*samples).pvalue
-        variance_info['levene'] = levene_pval > 0.05
-
-        # save the info in return object
-        output_info['levene'] = {'stat': levene_stat, 'p': levene_pval, 'equal_variances': variance_info['levene']}
-
-        # construct console output
-        levene_text = f"Results for samples in groups of '{grouping_variable}' for ['{target_variable}'] target variable:\n  ➡ statistic: {levene_stat}\n  ➡ p-value: {levene_pval}\n{(f'  ∴ Equal variances: Yes (H0 cannot be rejected)' if variance_info['levene'] else f'  ∴ Equal variances: No (H0 rejected)')}\n\n"
-        levene_title = f"< EQUAL VARIANCES TESTING: LEVENE >\n\n"
-        levene_tip = "☻ Tip: The Levene test is robust against departures from normality, making it suitable for a wide range of distributions, including those that are not normally distributed. It is preferred when you suspect that your data may not adhere to the normality assumption.\n"
-
-        # output & return
-        if method == 'levene':
-            print(levene_title, levene_text, levene_tip)
-            return output_info if not pipeline else variance_info['levene']
-
-    if method in ['fligner', 'consensus']:
-        fligner_stat, fligner_pval = fligner(*samples).statistic, fligner(*samples).pvalue
-        variance_info['fligner'] = fligner_pval > 0.05
-
-        # save the info in return object
-        output_info['fligner'] = {'stat': fligner_stat, 'p': fligner_pval, 'equal_variances': variance_info['fligner']}
-
-        # construct console output
-        fligner_text = f"Results for samples in groups of '{grouping_variable}' for ['{target_variable}'] target variable:\n  ➡ statistic: {fligner_stat}\n  ➡ p-value: {fligner_pval}\n{(f'  ∴ Equal variances: Yes (H0 cannot be rejected)' if variance_info['fligner'] else f'  ∴ Equal variances: No (H0 rejected)')}\n\n"
-        fligner_title = f"< EQUAL VARIANCES TESTING: FLIGNER-KILLEEN >\n\n"
-        fligner_tip = "☻ Tip: The Fligner-Killeen test is a non-parametric alternative that is less sensitive to departures from normality compared to the Bartlett test. It's a good choice when dealing with data that might not be normally distributed, offering robustness similar to the Levene test but through a different statistical approach.\n"
-
-        # output & return
-        if method == 'fligner':
-            print(fligner_title, fligner_text, fligner_tip)
-            return output_info if not pipeline else variance_info['fligner']
-
-    if method in ['bartlett', 'consensus'] and normality_info:
-        bartlett_stat, bartlett_pval = bartlett(*samples).statistic, bartlett(*samples).pvalue
-        variance_info['bartlett'] = bartlett_pval > 0.05
-
-        # save the info in return object
-        output_info['bartlett'] = {'stat': bartlett_stat, 'p': bartlett_pval, 'equal_variances': variance_info['bartlett']}
-
-        # construct console output
-        bartlett_text = f"Results for samples in groups of '{grouping_variable}' for ['{target_variable}'] target variable:\n  ➡ statistic: {bartlett_stat}\n  ➡ p-value: {bartlett_pval}\n{(f'  ∴ Equal variances: Yes (H0 cannot be rejected)' if variance_info['bartlett'] else f'  ∴ Equal variances: No (H0 rejected)')}\n\n"
-        bartlett_title = f"< EQUAL VARIANCES TESTING: BARTLETT >\n\n"
-        bartlett_tip = "☻ Tip: The Fligner-Killeen test is a non-parametric alternative that is less sensitive to departures from normality compared to the Bartlett test. It's a good choice when dealing with data that might not be normally distributed, offering robustness similar to the Levene test but through a different statistical approach.\n"
-
-        # output & return
-        if method == 'bartlett':
-            print(bartlett_title, bartlett_text, bartlett_tip)
-            return output_info if not pipeline else variance_info['bartlett']
-
-    if method == 'consensus':
-        # the logic is that more than half of the tests need to give True for the consensus to be True
-        variance_results = [variance_bool for variance_bool in variance_info.values()]
-        true_count = variance_results.count(True)
-        false_count = variance_results.count(False)
-        half_point = 1.5 if len(variance_results) == 3 else 1
-        if true_count > half_point:
-            consensus_percent = (true_count / len(variance_results)) * 100
-            variance_consensus_text = f"  ➡ Result: Consensus is reached.\n  ➡ {consensus_percent}% of tests suggest equal variance between samples. *\n\n* Detailed results of each test are provided below.\n"
-
-            # used only within predict_hypothesis() pipeline
-            variance_consensus = True
-
-        elif true_count < half_point:
-            consensus_percent = (false_count / len(variance_results)) * 100
-            variance_consensus_text = f"  ➡ Result: Consensus is reached.\n  ➡ {consensus_percent}% of tests suggest unequal variance between samples. *\n\n* Detailed results of each test are provided below:\n"
-
-            # used only within predict_hypothesis() pipeline
-            variance_consensus = False
-
-        elif true_count == half_point:
-            # the logic is that we default to the decisions of levene - internal use only
-            variance_consensus_text = f"  ➡ Result: Consensus is not reached.\n\n∴ Please refer to the results of each test below:\n"
-
-            # used only within predict_hypothesis() pipeline
-            variance_consensus = variance_info['levene']
-
-        print(f"< VARIANCE TESTING: CONSENSUS >\nThe consensus method bases its conclusion on 2-3 tests: Levene test, Fligner-Killeen test, Bartlett test. (Note: More than 50% must have the same outcome to reach consensus.)\n\n{variance_consensus_text}")
-        print(levene_title, levene_text, levene_tip)
-        print(fligner_title, fligner_text, fligner_tip)
-        print(bartlett_title, bartlett_text, bartlett_tip) if normality_info else f"\n\n< NOTE ON BARTLETT >\nBartlett was not used in consensus as no normality info has been provided or data is non-normal. Accuracy of Bartlett's test results rely heavily on normality."
-
-        return output_info if not pipeline else variance_consensus
-
-
-equal_variances_info = evaluate_variance(test_df, variable, grouping)
-
-# DONE! TODO: simplify output_info structure on below func (considering only one test will be outputted
-# TODO: make evaluate_shape stand on it's own two feet OR integrate into the performer
-# TODO: make evaluate_frequency stand on it's own two feet OR integrate into the performer
-
-
-def perform_hypothesis_testing_numeric(df, target_variable, grouping_variable, normality_bool, equal_variances_bool):
     groups = df[grouping_variable].unique().tolist()
     samples = [df[df[grouping_variable] == group][target_variable] for group in groups]
 
