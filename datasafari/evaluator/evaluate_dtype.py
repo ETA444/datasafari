1427c02a432d5782b25c8b3609635d75dd213c64
George <georgedreemer@proton.me>
2024-04-07 16:31:43 +0200

    Add datetime and text evaluation functionality

--- datasafari/evaluator/evaluate_dtype.py
+++ datasafari/evaluator/evaluate_dtype.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from pandas.api.types import is_numeric_dtype, is_string_dtype, is_datetime64_any_dtype
 
 
-def evaluate_dtype(df: pd.DataFrame, col_names: list, max_unique_values_ratio: float = 0.05, min_unique_values: int = 10, output: str = 'dict'):
+def evaluate_dtype(df: pd.DataFrame, col_names: list, max_unique_values_ratio: float = 0.05, min_unique_values: int = 10, string_length_threshold: int = 50, output: str = 'dict'):
     """
     Evaluates and categorizes data types of specified columns in a DataFrame, with enhanced handling for numerical data that may functionally serve as categorical data.
 
@@ -99,37 +100,32 @@ def evaluate_dtype(df: pd.DataFrame, col_names: list, max_unique_values_ratio: f
     if output not in valid_outputs:
         raise ValueError(f"evaluate_dtype(): Invalid output '{output}'. Valid options are: {', '.join(valid_outputs)}")
 
+    # Main Function
     data_type_dictionary = {}
-    numerical_type_list = []
-    categorical_type_list = []
     for col in col_names:
-
-        if df[col].dtype.name == 'category':
-            data_type_dictionary[col] = 'categorical'
-            numerical_type_list.append(False)
-            categorical_type_list.append(True)
-
-        elif df[col].dtype.kind in 'iuf':  # Integer, unsigned integer, float
+        if is_numeric_dtype(df[col]):
             num_unique_values = df[col].nunique()
             total_values = len(df[col])
-
             if (num_unique_values <= min_unique_values) or (num_unique_values / total_values <= max_unique_values_ratio):
                 data_type_dictionary[col] = 'categorical'
-                numerical_type_list.append(False)
-                categorical_type_list.append(True)
             else:
                 data_type_dictionary[col] = 'numerical'
-                numerical_type_list.append(True)
-                categorical_type_list.append(False)
+        elif is_string_dtype(df[col]):
+            avg_str_length = df[col].dropna().apply(len).mean()
+            if avg_str_length > string_length_threshold or df[col].nunique() / len(df[col]) > max_unique_values_ratio:
+                data_type_dictionary[col] = 'text'
+            else:
+                data_type_dictionary[col] = 'categorical'
+        elif is_datetime64_any_dtype(df[col]):
+            data_type_dictionary[col] = 'datetime'
         else:
-            # Default to categorical for other data types (e.g., string objects)
             data_type_dictionary[col] = 'categorical'
-            numerical_type_list.append(False)
-            categorical_type_list.append(True)
 
     if output.lower() == 'dict':
         return data_type_dictionary
     elif output.lower() == 'list_n':
+        numerical_type_list = [dtype == 'numerical' for dtype in data_type_dictionary.values()]
         return numerical_type_list
     elif output.lower() == 'list_c':
+        categorical_type_list = [dtype.startswith('categorical') for dtype in data_type_dictionary.values()]
         return categorical_type_list
