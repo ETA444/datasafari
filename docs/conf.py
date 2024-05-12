117ad57181330a5ef576434691b196ea81995a17
George <georgedreemer@proton.me>
2024-05-12 18:09:58 +0300

    Implement dark theme extension for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,10 @@ import datasafari
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx_rtd_dark_mode']
+
+# sphinx_rtd_dark_mode option: user starts in dark mode
+default_dark_mode = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
