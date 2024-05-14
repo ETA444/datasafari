c728549e270019aef88220b43e4588baee5754d4
George <georgedreemer@proton.me>
2024-05-14 20:42:14 +0300

    Implement final dark mode solution

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ release = datasafari.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -87,7 +87,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_light_dark'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further. For a list of options available for each theme, see the
@@ -97,7 +98,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': True,
     'vcs_pageview_mode': 'edit',
-    'style_nav_header_background': 'white',
+    # 'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
@@ -106,12 +107,17 @@ html_theme_options = {
     'titles_only': False
 }
 
-html_logo = "_static/test_logo_pic444.png"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# html_css_files = [
+#    'css/rtd_dark.css',
+# ]
+
+# sphinx_rtd_dark_mode option: user starts in dark mode
+# default_dark_mode = True
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -126,7 +132,7 @@ html_sidebars = {
 }
 
 # Optional logo. Uncomment the line below if you have a logo.
-# html_logo = "_static/my_logo.png"
+html_logo = "_static/my_logo.png"
 
 # If you have your own conf.py file, it's a good idea to merge this info with it,
 # ensuring that all settings are included correctly.
