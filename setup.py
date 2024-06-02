af78cfd9635118ba97f8aa17c10c1bd5e7576ed4
George <georgedreemer@proton.me>
2024-06-02 22:28:36 +0300

    Remove py37 support

--- setup.py
+++ setup.py
@@ -36,14 +36,13 @@ dev_requirements = [
 setup(
     author="George Dreemer",
     author_email='georgedreemer@proton.me',
-    python_requires='>=3.7, <4',
+    python_requires='>=3.8, <4',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
