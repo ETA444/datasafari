0e34195c9127a0f3fdb961939a2552db733528c6
George <georgedreemer@proton.me>
2024-06-02 22:56:36 +0300

    Remove support for Python 3.8

--- setup.py
+++ setup.py
@@ -36,14 +36,13 @@ dev_requirements = [
 setup(
     author="George Dreemer",
     author_email='georgedreemer@proton.me',
-    python_requires='>=3.8, <4',
+    python_requires='>=3.9, <4',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
